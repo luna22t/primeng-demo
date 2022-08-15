9eccb53eb41ce40db7141a411e8cf44b9629337b
Nguyen Tu <nduytu87@gmail.com>
2022-08-15 09:37:33 +0800

    appendTo body

--- tieredmenu/src/app/app.module.ts
+++ tieredmenu/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TieredMenuModule } from 'primeng/tieredmenu';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, TieredMenuModule],
+  imports: [
+    BrowserModule,
+    TieredMenuModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
